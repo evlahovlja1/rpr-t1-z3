4aa6e434088fe7397f942d3d7b0667acb99144e9
Elvir Vlahovljak <evlahovlja1@etf.unsa.ba>
2020-10-27 20:19:12 +0100

    kostur testova

---
+++ test/ba/unsa/etf/rpr/SatTest.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SatTest {
+
+    @Test
+    void sljedeci() {
+    }
+
+    @Test
+    void prethodni() {
+    }
+
+    @Test
+    void pomjeriZa() {
+    }
+
+    @Test
+    void ispisi() {
+    }
+
+    @Test
+    void getSati() {
+    }
+
+    @Test
+    void getMinute() {
+    }
+
+    @Test
+    void getSekunde() {
+    }
+}
